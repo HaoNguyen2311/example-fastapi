57992bcb273e2daf2dde74c2755b1eae0d062fde
HaoNguyen <hao.nguyen@ucars.sg>
2022-08-28 09:46:54 +0700

    change name field

--- app/models.py
+++ app/models.py
@@ -15,7 +15,8 @@ class Post(Base):
   content = Column(String, nullable = False)
   published = Column(Boolean, server_default = 'TRUE',nullable = False) 
   create_at = Column(TIMESTAMP(timezone=True), nullable = False, server_default = text('now()'))
-  user_id = Column(Integer,ForeignKey('user.id',ondelete="CASCADE") ,nullable = False)
+  owner_id = Column(Integer, ForeignKey(
+      'user.id', ondelete="CASCADE"), nullable=False)
   
   #  owner = relationship('class User')
   owner = relationship('User')
